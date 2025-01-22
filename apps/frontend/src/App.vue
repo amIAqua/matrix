f6c1e06e4f06e636ac00bd7f50dd08298ec8ebea
amIAqua <blackcurrantbush@gmail.com>
2025-01-22 22:02:23 +0100

    FE UI library setup

--- apps/frontend/src/App.vue
+++ apps/frontend/src/App.vue
@@ -2,5 +2,5 @@
 </script>
 
 <template>
-    hello penis
+    hello
 </template>
--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -1,4 +1,15 @@
 import { createApp } from 'vue';
 import App from 'src/App.vue';
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import 'src/app/styles/global.css';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
+
+app.mount('#app');
